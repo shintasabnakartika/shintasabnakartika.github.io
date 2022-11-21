d815892852f5c5affe1b81c6468b9a4aca21643d
shintasabnakartika <f8624shintasabnakartika>
2022-11-21 11:11:20 +0700

    Mengubah file

--- js/script.js
+++ js/script.js
@@ -1,53 +1,55 @@
 // event pada saat link di klik
-$('.page-scroll').on('click', function(){
-    // ambil isi href
-    var tujuan = $(this).attr('href');
-    
-    // tangkap elemen
-    var elemenTujuan = $(tujuan);
-    
-    // pindahkan scroll
-    // $('body').animate({
-    //     scrollTop: elemenTujuan.offset().top - 50
-    // }, 1000, 'easeInOutExpo');
+$(".page-scroll").on("click", function () {
+  // ambil isi href
+  var tujuan = $(this).attr("href");
 
-    // e.preventDefault();
+  // tangkap elemen
+  var elemenTujuan = $(tujuan);
+
+  // pindahkan scroll
+  $("body").animate(
+    {
+      scrollTop: elemenTujuan.offset().top - 50,
+    },
+    1000,
+    "easeInOutExpo"
+  );
+
+  e.preventDefault();
 });
 
-
 // ABOUT
-$(window).on('load', function(){
-    $('.pkiri').addClass('pMuncul');
-    $('.pkanan').addClass('pMuncul');
+$(window).on("load", function () {
+  $(".pkiri").addClass("pMuncul");
+  $(".pkanan").addClass("pMuncul");
 });
 // AKHIR ABOUT
 
 // parallax
-$(window).scroll(function(){
-    var wScroll = $(this).scrollTop();
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
 
-    // JUMBOTRON
-    $('.jumbotron img').css({
-        'transform' : 'translate(0px, '+ wScroll/4 +'%)' 
+  // JUMBOTRON
+  $(".jumbotron img").css({
+    transform: "translate(0px, " + wScroll / 4 + "%)",
+  });
+
+  $(".jumbotron h1").css({
+    transform: "translate(0px, " + wScroll / 2 + "%)",
+  });
+
+  $(".jumbotron p").css({
+    transform: "translate(0px, " + wScroll / 1.2 + "%)",
+  });
+  // AKHIR JUMBOTRON
+
+  // PORTFOLIO
+  if (wScroll > $(".portfolio").offset().top - 250) {
+    $(".portfolio .thumbnail").each(function (i) {
+      setTimeout(function () {
+        $(".portfolio .thumbnail").eq(i).addClass("muncul");
+      }, 300 * (i + 1));
     });
-
-    $('.jumbotron h1').css({
-        'transform' : 'translate(0px, '+ wScroll/2 +'%)' 
-    });
-
-    $('.jumbotron p').css({
-        'transform' : 'translate(0px, '+ wScroll/1.2 +'%)' 
-    });
-    // AKHIR JUMBOTRON
-
-
-    // PORTFOLIO
-    if( wScroll > $('.portfolio').offset().top - 250) {
-        $('.portfolio .thumbnail').each(function(i){
-            setTimeout(function(){
-                $('.portfolio .thumbnail').eq(i).addClass('muncul');
-            }, 300 * (i+1));
-        });
-    }
-    // AKHIR PORTFOLIO
+  }
+  // AKHIR PORTFOLIO
 });
